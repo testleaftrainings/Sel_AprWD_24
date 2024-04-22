c24bccc226062c0fcb99a9f61015feeb783a6e1a
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-04-22 12:41:46 +0530

    Add files via upload

---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		
+		//dataType variableName= value;
+		//2 types of variable
+		//local variable & global variable
+		//variable -it should start with lowercase following letter its should be upper case
+		//browser
+		
+		int tabOpened=2;
+		float version=121.34f;//end value you need f or F
+		char firstLetter='C';
+		double value=234.67;
+		boolean loginSuccessful = true;
+		String browserName="Chrome";
+		
+		System.out.println("Number of opened tab :"+tabOpened);
+		System.out.println("Browser Version :"+version +"\n"
+		+"FirstLetter of the browser :"+firstLetter);
+		
+		
+	}
+
+}
---
+++ week1/day1/SampleCode.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class SampleCode {
+
+	//main-ctrl+space
+	public static void main(String[] args) {
+		
+		//syso-ctrl+space
+		System.out.println("Welcome to TestLeaf");
+	}
+}
