06115024b4e8f8c9ae61bc2e0a1cb05f91b816c5
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-04-25 12:59:18 +0530

    Add files via upload

--- week1/day4/LoginPage.java
+++ week1/day4/LoginPage.java
@@ -17,6 +17,17 @@ public class LoginPage {
 	 //driver.findElement(By.linkText("CRM/SFA")).click();
 	 driver.findElement(By.partialLinkText("CR")).click();
 	 
+	 
+	 //get title  of page
+	 String title = driver.getTitle();
+	 System.out.println(title);
+	 
+	 if(title.contains("Automation")) {
+		 System.out.println("login");
+	 }else {
+		 System.out.println("not login");
+	 }
+	 
 	 //java wait
 	 Thread.sleep(5000);
 	 
